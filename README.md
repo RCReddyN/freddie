0e883c6d78360637b657679a5ded0a4b0adee096
Ravi Chandra Reddy N <me.rcreddy@live.com>
2021-02-25 02:08:36 +0530

    v1.0

---
+++ README.md
@@ -0,0 +1,24 @@
+<h1>Meet Elugubanti!</h1>
+<div align="center" style="font-size:10px">
+<img src = "nother.jpg"><br>
+If you own this picture, request for removal by writing to rcreddy1997@gmail.com
+<br>
+<br>
+</div>
+<p>If you speak Telugu, you might think, "Why am I looking at a sloth bear?" But no. I mean, how can you think that way? How can a bear sit on your screen? You are just looking at a name and a picture of one. You might even wonder how on any planet this works? My dear friend, it is so simple. It works almost the same way as Firefox does - it has nothing to do with fire or a fox.</p>
+
+<p>But, hey, come on. Let me really tell you about why you are here. You are here to experience a masterpiece in making.</p> 
+
+<p><b>Elugubanti</b> is a light-weight browser written in Python. She renders only text from a webpage ignoring all CSS, fancy JavaScript, pictures, videos and basically all visual elements. So, now you get why I wrote "almost the same way as Firefox". </p>
+
+<p>Elugubanti is still in development. She is just a few days old. Who knows what she will grow into?</p>  
+<ul>
+<li> Start a browsing session by running the script "browser.py".</li>
+<li> Fetch a web page and display it by typing in a url.</li>
+<li> Navigate through open tabs using a "switch tabs" command.</li>
+<li> Traverse through the browsing history using a "back" command.</li>
+<li> Fetch a Wikipedia summary of a query (A query must not resemble a url).</li>
+<li> Close the browser using an "exit" command.</li>
+</ul>
+
+<footer>This project is based on an idea from a blog post written by <a href="http://web.eecs.utk.edu/~azh/blog/morechallengingprojects.html"> Austin Z. Henley</footer>
---
+++ browser.py
@@ -0,0 +1,106 @@
+import requests
+from bs4 import BeautifulSoup
+import colorama
+import wikipedia
+from collections import deque
+
+class Browser:
+    tabs = dict()
+    history = deque()
+
+    details = {"Who am I?": "Elugubanti",
+                "What am I?":   "A text-based browser",
+                "Who pets me?": "Ravi Chandra N Reddy",
+                "How old am I?": "Just a few hours"}
+    
+    def __init__(self):
+        colorama.init()
+
+    def read_page_from_cache(self, page):
+        self.display(self.tabs[page])
+
+    def write_page_to_cache(self, page, page_content):
+        self.tabs[page] = page_content
+
+    def parse_text_from_page(self, r):
+        if r.status_code == 200:
+            page_content = ''
+            soup = BeautifulSoup(r.content, 'html.parser')
+            tags = ['p', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ul', 'al', 'li']
+            for tag in soup.find_all(tags):
+                if tag.name == 'a':
+                    page_content += colorama.Fore.BLUE + tag.text + '\n'
+                elif tag.name in tags:
+                    page_content += colorama.Fore.WHITE + tag.text + '\n'
+            return page_content
+        elif r.status_code == 404:
+            return "404, Page Not Found"
+        elif r.status_code == 410:
+            return "410, Page Gone"
+        elif r.status_code == 500:
+            return "Internal Server Error"
+        elif r.status_code == 503:
+            return "Service Unavailable"
+        else:
+            if r.status_code // 100 != 3:
+                return str(r.status_code) + ", Error" 
+    
+    def display(self, page_content):
+        print(page_content + "\n")
+    
+    def close_browser(self):
+        exit()
+
+    def wiki(self, query):
+        self.display(wikipedia.summary(query))
+
+    def switch(self):
+        for tab in self.history:
+            self.display(tab)
+
+def main():
+    browser_instance = Browser()
+    while True:
+        input_url = input()
+        if input_url == "exit":
+            browser_instance.close_browser()
+        elif input_url == "about":
+            for key in browser_instance.details:
+                print(key, ":", browser_instance.details[key])
+        elif input_url == "back":
+            if not browser_instance.history:
+                browser_instance.close_browser()
+            elif len(browser_instance.history) == 1:
+                browser_instance.history.pop()
+                browser_instance.display("Your history is Empty")
+            else:
+                    browser_instance.history.pop()
+                    previous_page = browser_instance.history.pop()
+                    browser_instance.display(browser_instance.read_page_from_cache(previous_page))
+                    browser_instance.history.append(previous_page)
+        elif input_url == "switch tabs":
+            if not browser_instance.history:
+                browser_instance.display("No other tabs open")
+            else:
+                browser_instance.switch()
+                continue
+        elif "." not in input_url:
+            browser_instance.wiki(input_url)
+        else:
+            if input_url.startswith("http://"):
+                input_url = input_url[len("http://"):]
+            if not input_url.startswith("https://"):
+                input_url = "https://" + input_url
+            if input_url in browser_instance.history:
+                page_content = browser_instance.read_page_from_cache(input_url)
+                browser_instance.display(page_content)
+                browser_instance.history.append(input_url)
+            else:
+                r = requests.get(input_url)
+                page_content = browser_instance.parse_text_from_page(r)
+                browser_instance.display(page_content)
+                browser_instance.write_page_to_cache(input_url, page_content)
+                browser_instance.history.append(input_url)
+
+if __name__ == "__main__":
+    main()
